c1573dd7f74f9c4cff18fb6d5d758708079aa259
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2023-06-25 18:03:21 +0300

    update footer

--- real-app-front/src/components/footer.jsx
+++ real-app-front/src/components/footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="container border-top pt-3 py-2 text-center">
+    <footer className="border-top pt-3 py-2 text-center fw-bold fs-5">
       <span>
-        Real <i className="i bi bi-geo-fill ">App</i>
+        Real <i className="bi bi-boxes "></i> App
       </span>
-      <span className="mx-2">&copy;</span>
+      <span className="mx-2 ">&copy;</span>
       <span>{new Date().getFullYear()}</span>
     </footer>
   );
